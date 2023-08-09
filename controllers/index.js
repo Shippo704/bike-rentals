7dc709f9023f18f4c46d48aa16fdd55989526985
Shippo704 <matt.taylor704@yahoo.ca>
2023-08-09 18:51:55 -0400

    linked the login and rental routes through the controller index.js. Added main.handlebars to views

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const loginRoutes = require('./loginRoutes');
+const rentalRoutes = require('./rentalRoutes');
 
 router.use('/', homeRoutes);
+router.use('/', loginRoutes);
+router.use('/', rentalRoutes);
 
 module.exports = router;
