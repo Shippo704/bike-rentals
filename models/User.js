552452d13ac2a3fa14d3053dc6fa96ad13b0f41b
HungQuocDang <129162404+HungQuocDang@users.noreply.github.com>
2023-08-09 20:07:26 -0400

    added to User.js encryption

--- models/User.js
+++ models/User.js
@@ -50,7 +50,18 @@ User.init (
             }
         }
     },
+
+
+
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              newUserData.creditCard = await cryptr.encrypt(newUserData.creditCard, 10 );
+              return newUserData;
+            },    
+       
+          },
         sequelize,
         timestamps: false,
         freezeTableName: true,
