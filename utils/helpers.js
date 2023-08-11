94846700ac86e69372fcc169908ef4633ce29a6a
Shippo704 <matt.taylor704@yahoo.ca>
2023-08-10 20:30:49 -0400

    added helper function to check if user is already logged in. added function to rentalRoutes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,11 @@
-module.exports = {};
+// checks if user is already logged in
+const loggedIn = (req, res, next) => {
+    if (req.session.loggedIn) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = loggedIn;
