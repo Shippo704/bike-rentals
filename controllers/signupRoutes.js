1336bfbad44f80b94e2e8a96a1527623e2152d6b
Shippo704 <131291705+Shippo704@users.noreply.github.com>
2023-08-16 00:23:16 -0400

    Update signupRoutes.js
    signup will stay logged in after signing up

--- controllers/signupRoutes.js
+++ controllers/signupRoutes.js
@@ -6,13 +6,13 @@ router.post('/signup', async (req, res) => {
     try {
       const userData = await User.create(req.body);
   
-      // req.session.save(() => {
-      //   req.session.user_id = userData.id;
-      //   req.session.loggedIn = true;
+       req.session.save(() => {
+         req.session.user_id = userData.id;
+         req.session.loggedIn = true;
   
         res.status(200).json(userData);
         console.log("signup OK");
-      //});
+      });
     } catch (err) {
       res.status(400).json(err);
     }
